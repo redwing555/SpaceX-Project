5d11e8b4dabd36a49ef95e44286cebaaa1f42c0b
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-08 14:23:44 +0100

    Create the Navbar for the project

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,11 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './navbar';
+
+const App = () => (
+  <>
+    <Navbar />
+  </>
+);
 
 export default App;
