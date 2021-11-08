648021ce73414fee1018b8812abe82e3bc67cfa8
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-08 14:31:46 +0100

    Fix Eslint error

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './navbar';
+import Navbar from './Navbar';
 
 const App = () => (
   <>
