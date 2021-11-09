f61fc256b148f76ffff8badf689f524086d48f15
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-09 13:26:07 +0100

    Add actions, action creators and additional switch cases for reserve missions and cancel missions

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,4 +1,7 @@
 const FETCH_MISSIONS = 'SpaceX-Project/books/FETCH_MISSIONS';
+const RESERVE_MISSIONS = 'SpaceX-Project/books/RESERVE_MISSIONS';
+const CANCEL_MISSIONS = 'SpaceX-Project/books/RESERVE_MISSIONS';
+
 const MISSIONS_URL = 'https://api.spacexdata.com/v3/missions';
 const initialState = [];
 
@@ -11,10 +14,37 @@ export const fetchAllMissions = () => async (dispatch) => {
   });
 };
 
+export const reserveMission = (id) => ({
+  type: RESERVE_MISSIONS,
+  id,
+});
+
+export const cancelMission = (id) => ({
+  type: CANCEL_MISSIONS,
+  id,
+});
+
 const missionsReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_MISSIONS:
       return [action.data];
+
+    case RESERVE_MISSIONS: {
+      const newState = state.map((rocket) => {
+        if (rocket.id !== action.id) return rocket;
+        return { ...rocket, reserved: true };
+      });
+      return newState;
+    }
+
+    case CANCEL_MISSIONS: {
+      const newState = state.map((rocket) => {
+        if (rocket.id !== action.id) return rocket;
+        return { ...rocket, reserved: false };
+      });
+      return newState;
+    }
+
     default:
       return state;
   }
