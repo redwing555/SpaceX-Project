d42ca251bce31d02c716cdef32c7c7ffde636b3f
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-09 14:54:07 +0100

    Get the data stored in the store and pass as prop to the component that will render each mission

--- src/components/MissionElement.js
+++ src/components/MissionElement.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchAllMissions } from '../redux/missions/missions';
+import IndividualMission from './IndividualMission';
 
-const MissionElement = () => (<h1> This is my Mission</h1>);
-
+const MissionElement = () => {
+  const dispatch = useDispatch();
+  const missionsData = useSelector((state) => state.missionsReducer);
+  useEffect(() => {
+    dispatch(fetchAllMissions());
+  }, []);
+  return (
+    <>
+      {missionsData.map((item) => (<IndividualMission key={item.mission_id} data={item} />
+      ))}
+    </>
+  );
+};
 export default MissionElement;
