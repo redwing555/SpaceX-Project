6241fab6b44a883a582cdb2455d0bf3c91517cdf
Hichami Houssam <hichami.houssam@gmail.com>
2021-11-09 11:06:42 +0100

    seperating styles from js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from './planet.png';
-import './navbar.css';
+import '../styles/navbar.css';
 
 const Navbar = () => (
   <>
