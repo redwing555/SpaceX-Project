58652183971533438dea1908120599a21d3ce6ff
Hichami Houssam <hichami.houssam@gmail.com>
2021-11-09 16:52:46 +0100

    styling

--- src/components/IndividualRocket.js
+++ src/components/IndividualRocket.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../styles/individualRocket.css';
 
 /* eslint-disable  */
 const Rocket = ({ image, description, name }) => {
   return (
     <li>
         <div>
-      <h1>{name}</h1>
+      
       <img src={image} alt="img-rocket" width="250" height="160"/>
       </div>
+      <div>
+      <h3>{name}</h3>
       <p>{description}</p>
+      </div>
       
     </li>
   );
