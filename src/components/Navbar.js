468f048e07eda8b0e17b2cdd38902cb681f89073
Hichami Houssam <hichami.houssam@gmail.com>
2021-11-09 10:49:09 +0100

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from './planet.png';
+import './navbar.css';
 
 const Navbar = () => (
   <>
     <nav className="navigation-bar d-flex">
       <div className="links-bar d-flex">
-        <img src={logo} alt="Project Logo" />
-        <h1> Space Travelers&apos; Hub </h1>
-        <ul className="d-flex">
-          <li>
-            <NavLink to="/" className="links" style={(isActive) => ({ textDecoration: isActive ? 'underline' : 'none', color: isActive ? 'black' : 'red' })} exact> Rockets </NavLink>
-          </li>
-          <li>
-            <NavLink to="/missions" className="links" style={(isActive) => ({ textDecoration: isActive ? 'underline' : 'none', color: isActive ? 'black' : 'red' })}> Missions </NavLink>
-          </li>
-          <li>
-            <NavLink to="/profile" className="links" style={(isActive) => ({ textDecoration: isActive ? 'underline' : 'none', color: isActive ? 'black' : 'red' })}> Profile </NavLink>
-          </li>
-        </ul>
+        <div className="logo-header">
+          <img className="logo" src={logo} alt="Project Logo" />
+          <h1> Space Travelers&apos; Hub </h1>
+        </div>
+        <div>
+          <ul className="navlist d-flex">
+            <li>
+              <NavLink to="/" className="links" style={(isActive) => ({ textDecoration: isActive ? 'underline' : 'none' })} exact> Rockets </NavLink>
+            </li>
+            <li>
+              <NavLink to="/missions" className="links" style={(isActive) => ({ textDecoration: isActive ? 'underline' : 'none' })}> Missions </NavLink>
+            </li>
+            <li>
+              <NavLink to="/profile" className="links profile" style={(isActive) => ({ textDecoration: isActive ? 'underline' : 'none' })}> Profile </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   </>
