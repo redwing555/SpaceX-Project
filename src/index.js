2ddf016ebfa31d0c363be983b8c6299a041040ab
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-10 19:48:37 +0100

    Try solutions to Eslint error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import './styles/index.css';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './redux/configureStore';
 
