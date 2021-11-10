6f6fd0023758d64190593fdd62526a6dc0f0d32a
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-10 23:55:56 +0100

    Make all reserved missions show on the profile page

--- src/components/ProfileElement.js
+++ src/components/ProfileElement.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 
-const ProfileElement = () => (<h1> This is my Profile</h1>);
+const ProfileElement = () => {
+  const missionsData = useSelector((state) => state.missionsReducer);
+  return (
+    <ul>
+      {missionsData.filter((data) => data.reserved)
+        .map((item) => (<li key={item.mission_id}>{item.mission_name}</li>))}
+    </ul>
+  );
+};
 
 export default ProfileElement;
