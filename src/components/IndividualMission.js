715c1271e14e0c034ad34b8b374eef527d5eaf2b
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-10 14:17:15 +0100

    Restructure the layout of each missions using the medium fireframe template provided

--- src/components/IndividualMission.js
+++ src/components/IndividualMission.js
@@ -1,13 +1,14 @@
 const IndividualMission = (prop) => {
   const { data } = prop;
-  const { mission_id: missionId, mission_name: missionName, description } = data;
+  const { mission_name: missionName, description } = data;
   return (
     <>
-      <div>
-        {missionName}
-        {missionId}
-      </div>
-      <div>{description}</div>
+      <span>
+        <b>{missionName}</b>
+      </span>
+      <span className="description">{description}</span>
+      <span className="center">Not A member</span>
+      <span className="center"><button type="button">Join Mission</button></span>
     </>
   );
 };
