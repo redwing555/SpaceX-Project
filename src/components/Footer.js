34adf8d794e04bc5604ba5ef5e38b34ca624ed0b
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-12 12:58:28 +0100

    Change the file path to social media links to make sure their links are not broken

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,17 +13,17 @@ const Footer = () => (
         <div className="creators">
           <p>Houssam Hichami</p>
           <div className="icons">
-            <Link to="https://github.com/redwing555" target="_blank" rel="noopener noreferrer"><AiFillTwitterSquare className="git fab fa-github-square" /></Link>
-            <Link to="https://twitter.com/" target="_blank" rel="noopener noreferrer"><AiFillGithub className="twitter fab fa-twitter-square" /></Link>
-            <Link to="https://www.linkedin.com/in/hichami-houssam/" target="_blank" rel="noopener noreferrer"><AiFillLinkedin className="linkedin fab fa-linkedin" /></Link>
+            <Link to={{ pathname: 'https://github.com/redwing555' }} target="_blank" rel="noopener noreferrer"><AiFillTwitterSquare className="git fab fa-github-square" /></Link>
+            <Link to={{ pathname: 'https://twitter.com/' }} target="_blank" rel="noopener noreferrer"><AiFillGithub className="twitter fab fa-twitter-square" /></Link>
+            <Link to={{ pathname: 'https://www.linkedin.com/in/houssam-hichami/' }} target="_blank" rel="noopener noreferrer"><AiFillLinkedin className="linkedin fab fa-linkedin" /></Link>
           </div>
         </div>
         <div className="creators">
           <p>Francis Ugorji.N.</p>
           <div className="icons">
             <Link to="www.twitter.com" target="_blank" rel="noopener"><AiFillTwitterSquare className="twitter fab fa-twitter-square" /></Link>
-            <Link to="https://github.com/Gambit142" target="_blank" rel="noopener noreferrer"><AiFillGithub className="git fab fa-github-square" /></Link>
-            <Link to="https://www.linkedin.com/in/francis-ugorji/" target="_blank" rel="noopener noreferrer"><AiFillLinkedin className="linkedin fab fa-linkedin" /></Link>
+            <Link to={{ pathname: 'https://github.com/Gambit142' }} target="_blank" rel="noopener noreferrer"><AiFillGithub className="git fab fa-github-square" /></Link>
+            <Link to={{ pathname: 'https://www.linkedin.com/in/francis-ugorji/' }} target="_blank" rel="noopener noreferrer"><AiFillLinkedin className="linkedin fab fa-linkedin" /></Link>
           </div>
         </div>
       </div>
